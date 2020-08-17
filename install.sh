ad3a3bdaaabc376b2889de2869168ab2d93f8a65
ronakon <67315128+ronakon@users.noreply.github.com>
2020-08-17 16:28:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 apt-get update && apt-get upgrade -y
-apt-get install apt-transport-https curl && pt-get update
-apt-get install docker.io
+apt install apt-transport-https -y curl && pt-get update
+apt-get install docker.io -y
 systemctl start docker
 systemctl enable docker
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
