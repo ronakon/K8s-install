b1316fc336fea83dccadd105b2fcf55d63d3b34f
ronakon <67315128+ronakon@users.noreply.github.com>
2020-08-17 16:11:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,3 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 apt install kubeadm kubelet kubectl kubernetes-cni
 swapoff -a
-kubeadm init
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
-sudo kubeadm token create --print-join-command
